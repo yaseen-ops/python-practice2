995567d78bed83b9c16b0d5d6b4fd8bac62ea9d0
yaseen <mohammedbyasin@gmail.com>
2020-12-19 15:55:24 +0530

    learning scripts - section 2

---
+++ mathematic.py
@@ -0,0 +1,20 @@
+from math import *
+
+print( 3 + 5 )
+print( 4 * 5 - 12 )
+my_num = -8
+print(abs(my_num))
+print(pow(2,3))
+print(max(6,8))
+print(min(12,8))
+print(round(8.4))
+
+#below functions required to import math function
+print(floor(8.7))
+print(ceil(7.2))
+print(sqrt(64))
+
+user_input=input("what is your age : ")
+print("My age is " +user_input)
+result = int(user_input) - 23
+print(result)
---
+++ tuples.py
@@ -0,0 +1,9 @@
+# Tuples are unmodifiable unlike list
+
+numbers = (4, 5)
+#numbers[0] = 7     # We cannot do this as tuples cannot be modified
+print(numbers)
+
+numbers = [(4, 5), (5, 6)]  # keeping tuples inside a list can be modified but in the list level only
+numbers[0] = (7, 8)
+print(numbers)
